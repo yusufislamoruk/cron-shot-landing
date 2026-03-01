55732ee58c6a9bab175b11d03eafffc555791fb7
yusufislam-1 <yusufislamoruk@gmail.com>
2026-03-01 17:21:09 +0300

    Added Pricing, Support, Sign in,Sign up without any direction

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -14,15 +14,15 @@ import {
 
 export default function Header() {
     return (
-        <header className="flex items-center justify between px-8 py-4 bg-white border-b border-gray-100">
+        <header className="flex items-center justify-between px-8 py-4 bg-white border-b border-gray-100">
             <div className="flex-shrink-0">
                 <Logo />
             </div>
             <div className="hidden md:flex">
                 <NavigationMenu>
-                    <NavigationMenuList>
+                    <NavigationMenuList className="gap-3">
                         <NavigationMenuItem>
-                            <NavigationMenuTrigger className="text-gray-600 font-normal hover:text-gray-900 bg-trasnparent">Use Cases</NavigationMenuTrigger>
+                            <NavigationMenuTrigger className="text-gray-600 font-normal hover:text-gray-900 bg-transparent cursor-pointer">Use Cases</NavigationMenuTrigger>
                             <NavigationMenuContent>
                                 <div className="p-3 w-[190px] bg-white rounded-md shadow-lg flex flex-col gap-2">
                                     <div className="font-medium text-gray-600 hover:text-gray-900 cursor-pointer">
@@ -46,9 +46,21 @@ export default function Header() {
                                 </div>
                             </NavigationMenuContent>
                         </NavigationMenuItem>
+                        <NavigationMenuItem className="px-4 py-2 text-gray-600 font-normal hover:text-gray-900 bg-transparent cursor-pointer">
+                            Pricing
+                        </NavigationMenuItem>
+                        <NavigationMenuItem className="px-4 py-2 text-gray-600 font-normal hover:text-gray-900 bg-transparent cursor-pointer">
+                            Support
+                        </NavigationMenuItem>
                     </NavigationMenuList>
-                </NavigationMenu>
-                
+                </NavigationMenu>                
+            </div>
+            
+            <div className="flex items-center gap-6 text-gray-600 font-normal hover:text-gray-900 bg-transparent cursor-pointer">
+                Sign in
+                <Button className="bg-[#1C1C1E] text-white hover:bg-[#2C2C2E] rounded-md px-6 shadow-sm cursor-pointer">
+                    Sign up &rarr;
+                </Button>
             </div>
             
         </header>
