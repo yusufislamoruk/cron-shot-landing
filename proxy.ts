17d939c06bdd0de749587c4f90691b09c66673ba
yusufislamoruk <yusufislamoruk@gmail.com>
2026-03-11 17:58:58 +0300

    Fix: proxy file fix.

--- proxy.ts
+++ proxy.ts
@@ -10,7 +10,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    String.raw`/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)`,
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
 }
